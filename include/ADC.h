14cd7608ab2b466048157756ea524e574102a333
tolaneko24 <nekoyeuanh@gmail.com>
2026-05-23 23:35:16 +0700

    Create ADC.h

---
+++ include/ADC.h
@@ -0,0 +1,12 @@
+#ifdef  ADC_H
+#define ADC_H
+
+// ================= KHỐI ĐIỀU KHIỂN LCD I2C =================
+void LCD_Write_Nibble(uint8_t nibble, uint8_t rs);
+void LCD_Write_Byte(uint8_t data, uint8_t rs);
+void LCD_Command(uint8_t cmd);
+void LCD_Char(uint8_t data);
+void LCD_String(char *str);
+void LCD_Init(void);
+
+#endif
