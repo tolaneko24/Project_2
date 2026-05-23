1b795eb198ca46b6f55d16a0dedcabbc5053fcd2
tolaneko24 <nekoyeuanh@gmail.com>
2026-05-23 23:35:19 +0700

    Create LCD_I2C.h

---
+++ include/LCD_I2C.h
@@ -0,0 +1,12 @@
+#ifdef  LCD_I2C_H
+#define LCD_I2C_H
+
+// ================= KHỐI ĐIỀU KHIỂN LCD I2C =================
+void LCD_Write_Nibble(uint8_t nibble, uint8_t rs);
+void LCD_Write_Byte(uint8_t data, uint8_t rs);
+void LCD_Command(uint8_t cmd);
+void LCD_Char(uint8_t data);
+void LCD_String(char *str);
+void LCD_Init(void);
+
+#endif
