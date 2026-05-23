b5c672bfc0a42ac123ebc7152868f4b2adc8f580
tolaneko24 <nekoyeuanh@gmail.com>
2026-05-23 23:36:48 +0700

    Create Loading_ani.c

---
+++ src/Loading_ani.c
@@ -0,0 +1,27 @@
+#include "set_up.h"
+#include "Loading_ani.h"
+
+// ================= GIAO DIỆN KHỞI ĐỘNG =================
+void LCD_Loading_Animation(void) {
+    char buffer[17];
+
+    for (int i = 0; i <= 100; i++) {
+        sprintf(buffer, "Load %3d%%", i);
+
+        LCD_Command(0x80);
+        LCD_String(buffer);
+
+        LCD_Command(0xC0);
+
+        for (int j = 0; j < 16; j++) {
+            if (j < (i * 16) / 100)
+                LCD_Char(0xFF);
+            else
+                LCD_Char(0x20);
+        }
+
+        _delay_ms(30);
+    }
+
+    LCD_Command(0x01);
+}
