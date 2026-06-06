ffda547db71cdbc5130c58a07ef8efc36fb133aa
tolaneko24 <nekoyeuanh@gmail.com>
2026-06-06 17:48:27 +0700

    Update TWI.c

--- src/TWI.c
+++ src/TWI.c
@@ -1,25 +1,38 @@
 #include "TWI.h"
 #include "set_up.h"
 
+#define SCL_baudrate (((F_CPU / SCL_CLOCK) - 16 ) / 2)
+#define MASK_START ((1 << TWINT) | (1 << TWSTA) | (1 << TWEN))
+#define MASK_WRITE ((1 << TWINT) | (1 << TWEN))
+#define MASK_STOP ((1 << TWINT) | (1 << TWSTO) | (1 << TWEN))
+
 // ================= NGOẠI VI GIAO TIẾP I2C (TWI) =================
 void I2C_Init(void) {
-    TWSR = 0x00;
-    TWBR = ((F_CPU / SCL_CLOCK) - 16) / 2;
-    TWCR = (1 << TWEN);
+
+    write_reg(TWSR, 0x00);
+    write_reg(TWBR, SCL_baudrate);
+    write_reg(TWCR, TWEN);
+
 }
 
 void I2C_Start(void) {
-    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
-    while (!(TWCR & (1 << TWINT)));
+
+    write_reg(TWCR, MASK_START) ;
+    while (!check_bit(TWCR, TWINT));
+
 }
 
 void I2C_Write(uint8_t data) {
-    TWDR = data;
-    TWCR = (1 << TWINT) | (1 << TWEN);
-    while (!(TWCR & (1 << TWINT)));
+
+    write_reg(TWDR, data);
+    write_reg(TWCR, MASK_WRITE);
+    while (!check_bit(TWCR, TWINT));
+
 }
 
 void I2C_Stop(void) {
-    TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
+
+    write_reg(TWCR, MASK_STOP);
     _delay_us(10);
+
 }
